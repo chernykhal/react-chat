ecb82a81ead371daa3df8d3a618f399a26e42e5a
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-26 22:14:14 +0500

    (feat) add Time component

---
+++ src/components/Time/index.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import ruLocale from "date-fns/locale/ru";
+
+const Time = ({ date }) =>
+  formatDistanceToNow(new Date(date), {
+    addSuffix: true,
+    locale: ruLocale,
+  });
+
+Time.propTypes = {
+  date: PropTypes.object,
+};
+
+export default Time;
