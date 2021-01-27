bfaa6f6cd1c9ed98565f7b9abecf975032771b46
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-27 17:36:09 +0500

    (feat) edit example data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,52 +10,45 @@ const Home = () => {
       <Dialogs
         dialogs={[
           {
-            user: {
-              fullname: "Алексей Степанов",
-              isOnline: false,
-              avatar:
-                "https://sun1-98.userapi.com/impf/c850732/v850732481/179e66/0jQ0DVIldDk.jpg?size=100x0&quality=96&crop=56,20,288,288&sign=2f5b62b1c86a4a0820f04372f89a6409&ava=1",
-            },
-            message: {
-              text:
-                "Мы все свидетельствуем Вам глубочайшее наше почтение и целуем Ваши ручки, дражайший папенька: Михайла, Федор, Варвара и Андрюша",
-              isReaded: true,
-              createdAt: new Date(),
-              isOutgoing: false,
-            },
-            unReaded: 3,
-          },
-          {
-            user: {
-              fullname: "Ринат Гильманов",
-              isOnline: true,
-              avatar:
-                "https://sun9-59.userapi.com/impg/Q0fZCr512TiBYJbg83G3uSKKe10UnMwOKyN8FQ/Izot0rXoCWE.jpg?size=1408x1400&quality=96&proxy=1&sign=3c33bb98d66d8091fe4b5f2e6a494156&type=album",
-            },
+            _id: Math.random(),
             message: {
               text: "За что игнор?",
               isReaded: true,
               createdAt: new Date(),
-              isOutgoing: false,
+              user: {
+                _id: 1,
+                fullname: "Ринат Гильманов",
+                isOnline: true,
+                avatar:
+                  "https://sun9-59.userapi.com/impg/Q0fZCr512TiBYJbg83G3uSKKe10UnMwOKyN8FQ/Izot0rXoCWE.jpg?size=1408x1400&quality=96&proxy=1&sign=3c33bb98d66d8091fe4b5f2e6a494156&type=album",
+              },
             },
             unReaded: 15,
           },
           {
-            user: {
-              fullname: "Владимир Путин",
-              isOnline: false,
-              avatar:
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Vladimir_Putin_%282020-02-20%29.jpg/1200px-Vladimir_Putin_%282020-02-20%29.jpg",
-            },
+            _id: Math.random(),
             message: {
-              text: "Боеголовки готовы, сэр!",
-              isReaded: false,
+              text: "Привет!",
+              isReaded: true,
               createdAt: new Date(),
-              isOutgoing: true,
+              user: {
+                _id: 2,
+                fullname: "Алексей Степанов",
+                isOnline: true,
+                avatar:
+                  "https://sun1-98.userapi.com/impf/c850732/v850732481/179e66/0jQ0DVIldDk.jpg?size=100x0&quality=96&crop=56,20,288,288&sign=2f5b62b1c86a4a0820f04372f89a6409&ava=1",
+              },
             },
-            unReaded: null,
+            unReaded: 5,
           },
         ]}
+        authUser={{
+          _id: 1,
+          fullname: "Александр Черных",
+          isOnline: true,
+          avatar:
+            "https://sun9-11.userapi.com/impf/c852136/v852136556/1584d0/tkiAUimwgSw.jpg?size=600x472&quality=96&proxy=1&sign=e7d6d0e05707900d90e9b7f665cdba9b&type=album",
+        }}
       />
       {/*<Message*/}
       {/*  avatar={*/}
