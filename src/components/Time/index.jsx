845c56f022216c50c7b8e9efa0d1a3c528ec5f4a
Alexander Chernykh <al.ed.chernykh@gmail.com>
2021-01-27 17:36:49 +0500

    (feat) edit Time component

--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -1,12 +1,19 @@
 import PropTypes from "prop-types";
-import formatDistanceToNow from "date-fns/formatDistanceToNow";
-import ruLocale from "date-fns/locale/ru";
+import differenceInHours from "date-fns/differenceInHours";
+import format from "date-fns/format";
 
-const Time = ({ date }) =>
-  formatDistanceToNow(new Date(date), {
-    addSuffix: true,
-    locale: ruLocale,
-  });
+const Time = ({ date }) => {
+  const dateDifference = differenceInHours(new Date(), date);
+  if (dateDifference < 24) {
+    return format(new Date(date), "p");
+  } else if (dateDifference >= 24 && dateDifference < 168) {
+    return format(new Date(date), "iiii");
+  } else if (dateDifference >= 168 && dateDifference < 8760) {
+    return format(new Date(date), "dd/MM");
+  } else if (dateDifference >= 8760) {
+    return format(new Date(date), "d/MM/y");
+  }
+};
 
 Time.propTypes = {
   date: PropTypes.object,
